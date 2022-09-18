4e3dd29266fd3141fa809b61c5f81feb387eb4bb
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-09-18 14:45:28 +0000

    [Add]ブートストラップhomeなどタグ追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
    resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
    resources :users, only: [:index, :edit, :update, :show, :create]
-
+   get "/homes/about" => "homes#about", as: "about"
     delete 'books/:id' =>'books#destroy', as: 'destroy_book'
-   
+
+
 
 
 end
