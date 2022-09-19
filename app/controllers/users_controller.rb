9057fce69315f238ae48fdf8660310ce322ff976
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-09-19 02:09:03 +0000

    [Add]空白の際のエラーメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+     flash[:notice] = "You have updated user successfully."
+     redirect_to user_path(@user)
+    else
+      @users = User.all
+      render :edit
+    end
   end
 
 
