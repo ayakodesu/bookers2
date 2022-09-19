2fcc4e86fa7f3bc29723edbf332b833441c73533
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-09-19 13:10:32 +0000

    [Add]追加訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum:50 }
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 2, maximum:20 }
+  validates :introduction, length: { maximum:50 }
 
 
   def get_profile_image
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about"
+  get "/home/about" => "homes#about", as: "about"
   devise_for :users
-
   resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :edit, :update, :show, :create]
   delete 'books/:id' =>'books#destroy', as: 'destroy_book'
